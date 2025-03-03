5c3c466db21762bae01df9d03f2e546e4873c196
JDJG <ghostzap123@gmail.com>
2025-03-03 00:51:59 -0500

    Oops, my bad fixed.

--- table.sql
+++ table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS SINCRONI_GLOBAL_CHAT(
    "server_id" BIGINT NOT NULL,
    "channel_id" BIGINT NOT NULL,
    "webhook_url" TEXT,
-   UNIQUE ("server_id", "channel_id"),
+   UNIQUE ("server_id", "channel_id")
 )
